245d5f3f2777d72e5b922bca50417dedcc2139e0
MilagrosAlbarengo <92064009+MilagrosAlbarengo@users.noreply.github.com>
2021-10-25 21:29:18 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+function area(base: number, altura: number): number {
+  let area: number = (base * altura) / 2;
+  return area;
+}
+console.log(area(1, 2));
+console.log(area(2, 4));
+console.log(area(3, 6));
+console.log(area(4, 8));
+console.log(area(5, 10));
+console.log(area(6, 12));
+console.log(area(7, 14));
